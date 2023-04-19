e3a3428215c3b057363398d0767fac82cd691333
xudean <xuda.it@outlook.com>
2023-04-19 11:39:37 +0800

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://virtuex.github.io/',
+  url: 'https://doc.xudean.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
